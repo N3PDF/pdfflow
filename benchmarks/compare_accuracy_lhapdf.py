dc914d357692a9a23dc84295cea27921a6696fd0
Stefano Carrazza <stefano.carrazza@cern.ch>
2020-03-02 10:25:01 +0100

    adding abs differenc

--- benchmarks/compare_accuracy_lhapdf.py
+++ benchmarks/compare_accuracy_lhapdf.py
@@ -22,11 +22,10 @@ def main(pdfname, pid):
     p = pdf.mkPDF(pdfname, DIRNAME)
     l_pdf = lhapdf.mkPDF(pdfname)
 
+    plt.figure(figsize=(16.0, 12.0))
+    plt.subplot(2, 2, 1)
     x = np.logspace(-9, 0, 1000, dtype=float)
     q2 = np.array([1.65, 3, 5, 10, 50, 1e2, 2e2, 5e2, 1e3, 2e3, 5e3], dtype=float)**2
-
-    plt.figure(figsize=(16.0, 6.0))
-    plt.subplot(1, 2, 1)
     for iq2 in q2:
         vl = np.array([l_pdf.xfxQ2(pid, ix, iq2) for ix in x])
         _,_,vp = p.xfxQ2(x, [iq2]*len(x), pid)
@@ -39,9 +38,22 @@ def main(pdfname, pid):
     plt.xlabel('x')
     plt.legend()
 
+    plt.subplot(2, 2, 3)
+    for iq2 in q2:
+        vl = np.array([l_pdf.xfxQ2(pid, ix, iq2) for ix in x])
+        _,_,vp = p.xfxQ2(x, [iq2]*len(x), pid)
+        plt.plot(x, np.abs(vp-vl), label='$Q^2=%.2e$' % iq2)
+    plt.xscale('log')
+    plt.yscale('log')
+    plt.ylim([1e-5, 10])
+    plt.title('%s, flav = %d' % (pdfname, pid))
+    plt.ylabel(r'$|f_{pdfflow} - f_{lhapdf}|$')
+    plt.xlabel('x')
+    plt.legend()
+
     x = np.array([1e-8, 1e-6, 1e-4, 0.01, 0.1, 0.2, 0.5, 0.8], dtype=float)
     q2 = np.logspace(0.5, 4.9, 1000, dtype=float)**2
-    plt.subplot(1, 2, 2)
+    plt.subplot(2, 2, 2)
     for ix in x:
         vl = np.array([l_pdf.xfxQ2(pid, ix, iq2) for iq2 in q2])
         _,_,vp = p.xfxQ2([ix]*len(q2), q2, pid)
@@ -54,7 +66,20 @@ def main(pdfname, pid):
     plt.xlabel('Q')
     plt.legend()
 
-    plt.savefig('diff_%s_flav%d.pdf' % (pdfname.replace('/','-'), pid), bbox_inches='tight')
+    plt.subplot(2, 2, 4)
+    for ix in x:
+        vl = np.array([l_pdf.xfxQ2(pid, ix, iq2) for iq2 in q2])
+        _,_,vp = p.xfxQ2([ix]*len(q2), q2, pid)
+        plt.plot(q2**0.5, np.abs(vp-vl), label='$x=%.2e$' % ix)
+    plt.xscale('log')
+    plt.yscale('log')
+    plt.ylim([1e-5, 10])
+    plt.title('%s, flav = %d' % (pdfname, pid))
+    plt.ylabel(r'$|f_{pdfflow} - f_{lhapdf}|$')
+    plt.xlabel('Q')
+    plt.legend()
+
+    plt.savefig('diff_%s_flav%d.png' % (pdfname.replace('/','-'), pid), bbox_inches='tight')
 
 
 if __name__ == "__main__":
