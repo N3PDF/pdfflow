a47ba4156e2aa25188f6841787a60dabe8d531b9
marcorossi5 <rssmrc.11@gmail.com>
2020-07-09 12:41:50 +0200

    alphaS first commit

--- src/pdfflow/configflow.py
+++ src/pdfflow/configflow.py
@@ -4,6 +4,8 @@ Define some constants, header style
 # Most of this can be moved to a yaml file without loss of generality
 import os
 
+import numpy as np
+
 # Set TF to only log errors
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "1"
 import tensorflow as tf
@@ -28,6 +30,7 @@ logger.addHandler(console_handler)
 # Define the tensorflow number types
 DTYPE = tf.float64
 DTYPEINT = tf.int32
+FMAX = tf.constant(np.finfo(np.float64).max, dtype=DTYPE)
 
 # The wrappers below transform tensors and array to the correct type
 def int_me(i):
