9a9e529683e5422f99b71bcb96c2aff9771824bc
Stefano Carrazza <stefano.carrazza@cern.ch>
2020-08-24 12:32:55 +0200

    adding some extra functions

--- capi/examples/example.c
+++ capi/examples/example.c
@@ -1,7 +1,16 @@
 // This file is part of PDFFlow
+#include <stdio.h>
 #include "pdfflow/pdfflow.h"
 
 int main() {
+    // load pdf
     mkpdf("NNPDF31_nlo_as_0118/0", "/usr/share/lhapdf/LHAPDF/");
+
+    // test xfxq2 and alphasq2
+    const double x = 0.1, q2 = 1.65;
+    for (int fl=-5; fl <=5; fl++)
+        printf("flv=%d - xfx = %f\n", fl, xfxq2(fl, x, q2));
+    printf("alphas(q2=%f) = %f\n", q2, alphasq2(q2));
+
     return 0;
 }
--- capi/src/wrapper.py
+++ capi/src/wrapper.py
@@ -2,10 +2,26 @@
 from cpdfflow import ffi
 from pdfflow import pflow
 
+pdf = None
 
 @ffi.def_extern()
 def mkpdf(fname, dirname):
     """Generate a PDF givena PDF name and a directory."""
     pdfset = ffi.string(fname).decode('utf-8')
     path = ffi.string(dirname).decode('utf-8')
+    global pdf
     pdf = pflow.mkPDF(pdfset, path)
+
+
+@ffi.def_extern()
+def xfxq2(pid, x, q2):
+    """Returns the xfxQ2 value for a given PID at specific x and q2."""
+    global pdf
+    return pdf.xfxQ2([pid], [x], [q2])
+
+
+@ffi.def_extern()
+def alphasq2(q2):
+    """Returns the alpha strong coupling at specific q2 value."""
+    global pdf
+    return pdf.alphasQ2([q2])
