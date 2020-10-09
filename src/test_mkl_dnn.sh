256408031d49ab1577e8102e357e7ad49ac9cf9a
marcorossi5 <rssmrc.11@gmail.com>
2020-10-09 14:14:55 +0200

    fix

--- src/test_mkl_dnn.sh
+++ src/test_mkl_dnn.sh
@@ -2,22 +2,22 @@
 BENCH=../benchmarks/test_performance_intel.py
 
 echo Generating samples ...;
-python ${BENCH} --mode generate;
+python ${BENCH} -m generate;
 
 echo Testing w/o MKL-DNN ...;
-python ${BENCH} --mode run --no_mkl;
+python ${BENCH} -m run --no_mkl;
 
 #echo Testing with inter_op 0 intra_op 0 ...;
 #python ${BENCH} --mode run --inter_op 0 --intra_op 0 --no_lhapdf;
 
 echo Testing with inter_op 1 intra_op 96 ...;
-python ${BENCH} --mode run --inter_op 1 --intra_op 96 --no_lhapdf;
+python ${BENCH} -m run --inter_op 1 --intra_op 96 --no_lhapdf;
 
 echo Testing with inter_op 2 intra_op 48 ...;
-python ${BENCH} --mode run --inter_op 2 --intra_op 48 --no_lhapdf;
+python ${BENCH} -m run --inter_op 2 --intra_op 48 --no_lhapdf;
 
 echo Testing with inter_op 3 intra_op 32 ...;
-python ${BENCH} --mode run --inter_op 3 --intra_op 32 --no_lhapdf;
+python ${BENCH} -m run --inter_op 3 --intra_op 32 --no_lhapdf;
 
 echo Plotting results ...;
-python ${BENCH} --plot
+python ${BENCH} -m plot
