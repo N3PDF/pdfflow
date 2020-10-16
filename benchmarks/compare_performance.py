ce870250803dafd98174bfcdad6e08dea9b49c9f
marcorossi5 <rssmrc.11@gmail.com>
2020-10-16 09:37:47 +0200

    add customizations for n_exp and n_points

--- benchmarks/compare_performance.py
+++ benchmarks/compare_performance.py
@@ -4,6 +4,8 @@ Benchmark script for LHAPDF comparison being hardware agnostic.
 - generate: generate the input points
 - run: run the experiment
 - plot: collect results and plot
+
+The user has to set the dictionary inside the code for making plots
 """
 import os
 import glob
@@ -20,12 +22,18 @@ import pdfflow.pflow as pdf
 from pdfflow.configflow import float_me
 
 parser = argparse.ArgumentParser()
+parser.add_argument("--dirname", "-d", default="../benchmarks/tmp", type=str,
+                    help="Folder where to place results and plots.")
 parser.add_argument("--pdfname", "-p", default="NNPDF31_nlo_as_0118/0", type=str,
                     help="The PDF set name/replica number.")
 parser.add_argument("--mode", default="generate", type=str,
                     help="generate/run/plot")
 parser.add_argument("--n_exp", default=10, type=int,
                     help="Number of experiments to average on.")
+parser.add_argument("--start_point", "-s", default=1e5, type=float,
+                    help="Start range of query array lengths.")
+parser.add_argument("--end_point", "-e", default=1e6, type=float,
+                    help="End range of query array lengths.")
 parser.add_argument("--n_points", default=20, type=int,
                     help="Number of different query array lengths.")
 parser.add_argument("--no_lhapdf", action="store_true",
@@ -33,14 +41,21 @@ parser.add_argument("--no_lhapdf", action="store_true",
 parser.add_argument("-t", "--tensorboard", action="store_true",
                     help="Enable tensorboard profile logging")
 parser.add_argument("--dev", default="GPU:*", type=str,
-                    help="pdfflow running device: CPU:0/GPU:<n,*>/TPU")
+                    help="pdfflow running device: CPU/GPU:<n,*>/TPU")
 parser.add_argument("--no_tex", action="store_true",
                     help="Don't render pyplot with tex")
 parser.add_argument("--fname", default="time.pdf", type=str,
                     help="Output plot file name")
 DIRNAME = (sp.run(["lhapdf-config", "--datadir"], stdout=sp.PIPE,
                   universal_newlines=True).stdout.strip("\n") + "/")
-DIRTMP = "../benchmarks/tmp/"
+
+
+def pprint(x):
+    """Print the given number in a compact formato for filenames"""
+    exponent = int(np.log10(x))
+    root = x/10**exponent
+
+    return "{:.1f}e{:d}".format(root, exponent)
 
 
 def set_env_vars(dev):
@@ -50,44 +65,43 @@ def set_env_vars(dev):
     """
     if "GPU" in dev:
         print("Running PDFFlow on GPU")
-        gpu, gpu_n = dev.split(":")
         gpus = [i for i in range(len(tf.config.list_physical_devices('GPU')))]
-        gpus.append("*")  # add the possibility to take all the GPUs
-        if not gpu_n in gpus:
-            raise AssertionError("Selected GPU not present on machine")
-        else:
-            if gpu_n != "*":
-                os.environ["CUDA_VISIBLE_DEVICES"] = gpu_n
+        if len(gpus) == 0:
+            raise AssertionError("No GPU found, please run with --dev CPU")
+        gpu, gpu_n = dev.split(":")
+        if gpu_n != "*":
+            os.environ["CUDA_VISIBLE_DEVICES"] = gpu_n
 
     if "CPU" in dev:
         print("Running PDFFlow on CPU")
         os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
 
-def load_run_inputs(pdfname, n_points, n_exp):
-    n = np.linspace(1e5, 1e6, n_points).astype(int).cumsum()
-
+def load_run_inputs(dirname, pdfname, points, n_exp):
     pdfname = "-".join(pdfname.split("/"))
 
-    fname = "".join([DIRTMP,  f"input_{pdfname}_{n_points}_{n_exp}_x.npy"])
+    start = pprint(points[0])
+    end = pprint(points[-1])
+
+    fname = os.path.join(dirname,
+                         "_".join(["input", f"{pdfname}", f"{start}",
+                                   f"{end}", f"{len(points)}", f"{n_exp}",
+                                   "x.npy"]))
     x = np.load(fname)
-    x = np.split(x.reshape([n_exp, -1]), n, axis=1)[:-1]
 
-    fname = "".join([DIRTMP,  f"input_{pdfname}_{n_points}_{n_exp}_q2.npy"])
+    fname = fname[:fname.rfind("_")] + "_q2.npy"
     q2 = np.load(fname)  # shape [n_exp, all draws]
-    q2 = np.split(q2.reshape([n_exp, -1]), n, axis=1)[:-1]
 
-    return x, q2
+    return x.reshape([n_exp, -1]), q2.reshape([n_exp, -1])
 
 
-def load_plot_inputs(fname, dev):
-    fname = "".join([DIRTMP,
-                     f"{fname}_{dev}.npy"])
+def load_plot_inputs(dirname, fname, dev):
+    fname = os.path.join(dirname, f"{fname}_{dev}.npy")
     res = np.load(fname)
 
-    n = res[:, -1]
-    mean = res[:, :-1].mean(1)
-    std = res[:, :-1].std(1)/np.sqrt(len(n))
+    n = res[-1]
+    mean = res[:-1].mean(0)
+    std = res[:-1].std(0)/np.sqrt(len(n))
 
     return {"n": n,
             "mean": mean,
@@ -116,7 +130,7 @@ def test_lhapdf(l_pdf, a_x, a_q2):
     return time() - start
 
 
-def accumulate_times(pdfname, points_exp_x, points_exp_q2, no_lhapdf, dev):
+def accumulate_times(pdfname, points_exp_x, points_exp_q2, n_query, no_lhapdf, dev):
     """
     Computes performance times
     Parameters:
@@ -150,12 +164,15 @@ def accumulate_times(pdfname, points_exp_x, points_exp_q2, no_lhapdf, dev):
     n = []
 
     for exp_x, exp_q2 in tqdm.tqdm(zip(points_exp_x, points_exp_q2)):
-        # iterate over n_points query lengths
+        # iterate over the experiments
         tp = []
         tl = None if no_lhapdf else []
-        n.append(exp_x.shape[-1])
-        for x, q2 in tqdm.tqdm(zip(exp_x, exp_q2)):
-            # iterate over the experiments
+
+        xs = np.split(exp_x, n_query)[:-1]
+        q2s = np.split(exp_q2, n_query)[:-1]
+
+        for x, q2 in tqdm.tqdm(zip(xs, q2s)):
+            # iterate over arrays of length query points
             tp.append(test_pdfflow(p, x, q2, strategy))
 
             if not no_lhapdf:
@@ -163,9 +180,8 @@ def accumulate_times(pdfname, points_exp_x, points_exp_q2, no_lhapdf, dev):
         t_pdf.append(tp)
         if not no_lhapdf:
             t_lha.append(tl)
-    # t_pdf is a list with shape [n_points, n_exp]
-    # n is a list with shape [n_points]
-    return np.array(n)[:, None], np.array(t_pdf), np.array(t_lha)
+    # t_pdf is a list with shape [n_exp, n_query]
+    return np.array(t_pdf), np.array(t_lha)
 
 
 def make_plots(fname, no_tex, args):
@@ -238,10 +254,10 @@ def make_plots(fname, no_tex, args):
     plt.close()
 
 
-def generate(pdfname, n_points, n_exp):
+def generate(dirname, pdfname, points, n_exp):
     print("Generating inputs...")
-    if not os.path.isdir(DIRTMP):
-        os.makedirs(DIRTMP)
+    if not os.path.isdir(dirname):
+        os.makedirs(dirname)
 
     p = pdf.mkPDF(pdfname, DIRNAME)
 
@@ -250,26 +266,31 @@ def generate(pdfname, n_points, n_exp):
     q2min = np.sqrt(np.exp(p.grids[0][0].log_q2min))
     q2max = np.sqrt(np.exp(p.grids[0][-1].log_q2max))
 
-    n = np.linspace(1e5, 1e6, n_points).astype(int)
     x = []
     q2 = []
-    for j in range(n_exp):
-        for i in n:
-            x.append(np.random.uniform(xmin, xmax, [i, ]))
+    for experiment in range(n_exp):
+        for point in points:
+            x.append(np.random.uniform(xmin, xmax, [point, ]))
             q2.append(np.exp(np.random.uniform(np.log(q2min),
-                                               np.log(q2max), [i, ])))
+                                               np.log(q2max), [point, ])))
     x = np.concatenate(x)
     q2 = np.concatenate(q2)
 
     pdfname = "-".join(pdfname.split("/"))
-    fname = "".join([DIRTMP,  f"input_{pdfname}_{n_points}_{n_exp}_x"])
+
+    start = pprint(points[0])
+    end = pprint(points[-1])
+    fname = os.path.join(dirname,
+                         "_".join(["input", f"{pdfname}", f"{start}",
+                                   f"{end}", f"{len(points)}", f"{n_exp}",
+                                   "x.npy"]))
     np.save(fname, x)
 
-    fname = "".join([DIRTMP,  f"input_{pdfname}_{n_points}_{n_exp}_q2"])
+    fname = fname[:fname.rfind("_")] + "_q2.npy"
     np.save(fname, q2)
 
 
-def run(pdfname, n_points, n_exp, no_lhapdf, dev, ext=None):
+def run(dirname, pdfname, points, n_exp, no_lhapdf, dev, ext=None):
     """
     Run the experiment
     It's user's responsibility to load the appropriate input .npy files,
@@ -280,26 +301,27 @@ def run(pdfname, n_points, n_exp, no_lhapdf, dev, ext=None):
 
     set_env_vars(dev)
 
-    x, q2 = load_run_inputs(pdfname, n_points, n_exp)
+    x, q2 = load_run_inputs(dirname, pdfname, points, n_exp)
+    n_query = points.cumsum()
 
-    n, res_pdf, res_lha = accumulate_times(pdfname, x, q2, no_lhapdf, dev)
+    res_pdf, res_lha = accumulate_times(pdfname, x, q2, n_query, no_lhapdf, dev)
 
     pdfname = "-".join(pdfname.split("/"))
     if ext == None:
-        fname = "".join(
-            [DIRTMP,  f"results_{pdfname}_{n_points}_{n_exp}_{dev}"])
+        fname = os.path.join(
+            dirname,  "_".join([f"results_{pdfname}_{pprint(points[0])}",
+                                f"{pprint(points[-1])}_{len(points)}",
+                                f"{n_exp}_{dev}"]))
     else:
-        fname = "".join(
-            [DIRTMP,  f"results_{pdfname}_{n_points}_{n_exp}_{ext}"])
-    np.save(fname, np.concatenate([res_pdf, n], 1))
+        fname = fname[:fname.rfind("_")] + f"_{ext}"
+    np.save(fname, np.concatenate([res_pdf, n_query[None]], 0))
 
     if not no_lhapdf:
-        fname = "".join(
-            [DIRTMP,  f"results_{pdfname}_{n_points}_{n_exp}_lhapdf"])
-        np.save(fname, np.concatenate([res_lha, n], 1))
+        fname = fname[:fname.rfind("_")] + "_lhapdf"
+        np.save(fname, np.concatenate([res_lha, n_query[None]], 0))
 
 
-def plot(in_fname, out_fname, no_tex, args):
+def plot(dirname, in_fname, out_fname, no_tex, args):
     print("Collect results and plotting...")
     mpl.rcParams['text.usetex'] = not no_tex
     mpl.rcParams['savefig.format'] = 'pdf'
@@ -310,23 +332,27 @@ def plot(in_fname, out_fname, no_tex, args):
     mpl.rcParams['legend.fontsize'] = 18
 
     for arg in args:
-        arg.update(load_plot_inputs(in_fname, arg["ext"]))
+        arg.update(load_plot_inputs(dirname, in_fname, arg["ext"]))
     make_plots(out_fname, no_tex, args)
 
 
-def main(pdfname, mode, n_exp, n_points, no_lhapdf, tensorboard, dev, no_tex,
-         fname):
+def main(dirname, pdfname, mode, n_exp, start_point, end_point, n_points, no_lhapdf,
+         tensorboard, dev, no_tex, fname):
+    points = np.linspace(start_point, end_point, n_points).astype(int)
+    start = pprint(points[0])
+    end = pprint(points[-1])
+
     if mode == "generate":
-        generate(pdfname, n_points, n_exp)
+        generate(dirname, pdfname, points, n_exp)
     if mode == "run":
         if tensorboard:
             tf.profile.experimental.start('logdir')
-        run(pdfname, n_points, n_exp, no_lhapdf, dev)
+        run(dirname, pdfname, points, n_exp, no_lhapdf, dev)
         if tensorboard:
             tf.profile.experimental.stop('logdir')
     if mode == "plot":
         pdfname = "-".join(pdfname.split("/"))
-        in_name = f"results_{pdfname}_{n_points}_{n_exp}"
+        in_name = f"results_{pdfname}_{start}_{end}_{n_points}_{n_exp}"
         texpdf = "PDFFlow" if no_tex else r"\texttt{PDFFlow}"
         args = [
             {
@@ -336,13 +362,13 @@ def main(pdfname, mode, n_exp, n_points, no_lhapdf, tensorboard, dev, no_tex,
                 "marker": "o"
             },
             {
-                "ext": "CPU:0",
+                "ext": "CPU",
                 "label": "%s: cpu" % texpdf,
                 "color": "lime",
                 "marker": "^"
             }
         ]
-        plot(in_name, fname, no_tex, args)
+        plot(dirname, in_name, fname, no_tex, args)
 
 
 if __name__ == "__main__":
