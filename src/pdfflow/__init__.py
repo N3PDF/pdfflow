a10df025e0f3e8e803feff58136155e2742d2b30
marcorossi5 <rssmrc.11@gmail.com>
2022-03-15 10:54:08 +0100

    completely remove setup.py

--- src/pdfflow/__init__.py
+++ src/pdfflow/__init__.py
@@ -1,7 +1,10 @@
 """PDF interpolation with tensorflow"""
 # Expose mkPDF and the int_me, float_me functions
 # that way the log system is imported from the very beginning
+from importlib.metadata import metadata
 from pdfflow.configflow import int_me, float_me, run_eager
 from pdfflow.pflow import mkPDF, mkPDFs
 
-__version__ = "1.2.1"
+PACKAGE = 'pdfflow'
+
+__version__ = metadata(PACKAGE)['version']
